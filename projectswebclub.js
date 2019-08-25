170e008291ba5a411f0a51a6502c327ede9aecc8
sidITkid <49162646+sidITkid@users.noreply.github.com>
2019-08-26 01:11:58 +0530

    Add files via upload

---
+++ projectswebclub.js
@@ -0,0 +1,13 @@
+function ongoing()
+{
+    document.getElementById("project").style.background="yellow";
+    document.getElementById("head").innerHTML="Machine Learning Pipeline";
+    document.getElementById("labeltext").style.visibility="visible";
+}
+function past()
+{
+    document.getElementById("project").style.background = "bisque";
+    document.getElementById("head").innerHTML = "Heterogenous Parallel Computing with CUDA";
+    document.getElementById("labeltext").style.visibility = "visible";
+
+}
